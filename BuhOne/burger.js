f995331d53cd1393308dfe17f5502bc005f96115
Chebanro2807 <chebanro2807@ukr.net>
2021-02-01 15:35:10 +0200

    end_header

---
+++ BuhOne/burger.js
@@ -0,0 +1,27 @@
+class Nav {
+    constructor() {
+        this._menuEl = document.querySelector('.main_container');
+        this._hamburgerEl = document.querySelector('.transform-hamburger');
+        this._navEl = document.querySelector('.section_header');
+
+        this._hamburgerEl.addEventListener('click', this.toggle.bind(this));
+        this._menuEl.addEventListener('transitionend', this.endTransition.bind(this));
+    }
+
+    show() {
+        this._hamburgerEl.classList.add('transform-hamburger--transformed');
+        this._navEl.classList.add('menu__wrapper--open');
+    }
+
+    hide() {
+        this._hamburgerEl.classList.remove('transform-hamburger--transformed');
+        this._navEl.classList.remove('menu__wrapper--open');
+    }
+
+    toggle() {
+        this._hamburgerEl.classList.toggle('transform-hamburger--transformed');
+        this._navEl.classList.toggle('menu__wrapper--open');
+    }
+}
+
+new Nav();
