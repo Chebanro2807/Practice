a87106a84b7c257096de502d0456f32ec7876a6d
Chebanro2807 <chebanro2807@ukr.net>
2021-02-03 15:04:12 +0200

    main slider done

--- BuhOne/main_slider.js
+++ BuhOne/main_slider.js
@@ -2,6 +2,7 @@ class Slider {
     constructor(){
 
         this._currentSlide = 0;
+        this._stop = false;
 
         this._root = document.querySelector('.slider_wrap');
         this._slides = document.querySelectorAll('.item');
@@ -9,23 +10,43 @@ class Slider {
         this._nextBtn = document.querySelector('.slider__control-right');
         this._prevBtn = document.querySelector('.slider__control-left');
 
-        this._nextBtn.addEventListener('click', this.next.bind(this));
-        this._prevBtn.addEventListener('click', this.prev.bind(this));
+        this._nextBtn.addEventListener('click', this.userClick.bind(this, this.next.bind(this)));
+        this._prevBtn.addEventListener('click', this.userClick.bind(this, this.prev.bind(this)));
 
-        this.showNextAfter(5000);
+        this._allLi = document.querySelectorAll('.slider__indicators li');
+        this._allLi.forEach(dot => {
+            dot.addEventListener('click', this.userClick.bind(this, this.clickedDot.bind(this, parseInt(dot.getAttribute("data-slide"))))) // clickedDot , parseInt(dot.getAttribute("data-slide"))
+        });
+
+        this.showNextAfter(3000);
     }
 
     showNextAfter(ms) {
+        console.log(this._stop);
         return delay(ms)
-            .then(() => this.next())
-            .then(() => this.showNextAfter(ms));
+            .then(() => (!this._stop) ? this.next() : false)
+            .then(() =>  (!this._stop) ? this.showNextAfter(ms) : false);
+    }
+    
+
+    clickedDot(nextDot){
+        // console.log("clicked")
+        return this.show(nextDot);
+    }
+
+    userClick(func){
+        console.log("user")
+        this._stop = true;
+        func();
     }
 
     next() {
+        console.log("next")
         return this.show(this._currentSlide + 1);
     }
 
     prev() {
+        console.log("prev")
         return this.show(this._currentSlide - 1);
     }
 
@@ -40,12 +61,15 @@ class Slider {
         }
 
         this._currentSlide = nextSlideNumber;
+        // console.log(this._currentSlide, " - ", nextSlideNumber);
 
         this.render(previousSlideNumber,nextSlideNumber);
     }
 
     
     hideSlide(slideNumber){
+        let previousLi = document.querySelector('.slider__indicators li[data-slide="' + slideNumber + '"]');
+        previousLi.classList.remove('active');
         const slide = this._slides[slideNumber];
         slide.classList.add('item-hide');
     }
@@ -53,13 +77,15 @@ class Slider {
     showSlide(slideNumber){
         const slide = this._slides[slideNumber];
         slide.classList.remove('item-hide');
+        let nextLi = document.querySelector('.slider__indicators li[data-slide="' + slideNumber + '"]');
+        nextLi.classList.add('active');
     }
      
     render(previousSlideNumber,nextSlideNumber){
         if (previousSlideNumber!==nextSlideNumber){
             return Promise.all([
                 this.hideSlide(previousSlideNumber),
-                delay(0).then(() =>this.showSlide(nextSlideNumber))
+                this.showSlide(nextSlideNumber)
             ]);
         }
         return Promise.resolve();
