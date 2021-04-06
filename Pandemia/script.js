94b28231e837714f2580f7df9fa4fdbabe2022ed
Chebanro2807 <chebanro2807@ukr.net>
2021-04-06 17:21:10 +0300

    create 1 city

--- Pandemia/script.js
+++ Pandemia/script.js
@@ -4,10 +4,13 @@ class Game {
         this._game = document.querySelector('.game_container');
         this._buttonStart = document.querySelector('.start');
         this._buttonStart.addEventListener('click', this.showGame.bind(this));
+
+        this._worldMap=document.querySelector('.intro');
     }
     
     show() {
         this._game.classList.add('show');
+        this._worldMap.classList.add('show');
     }
 
     hide() {
@@ -17,6 +20,17 @@ class Game {
     showGame() {
         this.hide();
         this.show();
+        this.createCity()
+    }
+
+    createCity() {
+        let city = document.createElement('div');
+        city.classList.add('city');
+        city.setAttribute('id', 'id0');
+        let cityFunctions = document.createElement('div');
+
+
+        this._worldMap.append(city);
     }
 }
 
