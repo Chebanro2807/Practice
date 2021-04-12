5780be3d09005abce39bc90e999af6f02d2f510a
AddLokkes-Self <vetakk123@gmail.com>
2021-04-12 17:52:43 +0300

    change block card

--- Pandemia/script.js
+++ Pandemia/script.js
@@ -300,5 +300,31 @@ class Game {
     }
 }
 
+// class Indicator {
+//     constructor() {
+//         let indicators = document.querySelectorAll(".class-indicator");
+//         this._blue = document.querySelector("#blue-indicator");
+//         this._yellow = indicators[0];
+//         this._green = indicators[1];
+
+//         let defaultValue = 1;
+//         this._y = defaultValue;
+//         this._b = 1;
+//         this._g = defaultValue;
+//         this.updateYellowIndicator(defaultValue);
+//         this.updateBlueIndicator();
+//         this.updateGreenIndicator(this._g);
+
+//         let yellowBtn = document.querySelector("#yellow-btn");
+//         let blueBtn = document.querySelector("#blue-btn");
+//         let greenBtn = document.querySelector(".green-class-btn");
+
+//         yellowBtn.addEventListener("click", this.btnClick.bind(this, "y"));
+//         blueBtn.addEventListener("click", this.btnClick.bind(this, "b"));
+//         greenBtn.addEventListener("click", this.btnClick.bind(this, "g"));
+//     }
+// }
+
+// let flashIndicator = new Indicator();
 
 const pandemic = new Game();
